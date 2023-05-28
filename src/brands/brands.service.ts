141ef4e33f242643e000565ce9fefece6ef3337f
Donato Monzon <spartanhalokitty@gmail.com>
2023-05-28 15:25:04 -0600

    feat: :sparkles: create `brand` CRUD

---
+++ src/brands/brands.service.ts
@@ -0,0 +1,68 @@
+import { Injectable, NotFoundException } from '@nestjs/common'
+import { CreateBrandDto } from './dto/create-brand.dto'
+import { UpdateBrandDto } from './dto/update-brand.dto'
+import { Brand } from './entities/brand.entity'
+
+import { v4 as uuid} from 'uuid'
+
+@Injectable()
+export class BrandsService {
+
+  private brands: Brand[] = [
+    {
+      id: uuid(),
+      name: 'Toyota',
+      createAt: Date.now()
+    },
+    {
+      id: uuid(),
+      name: 'Nissan',
+      createAt: Date.now()
+    },
+    {
+      id: uuid(),
+      name: 'Tesla',
+      createAt: Date.now()
+    },
+  ]
+
+  create(createBrandDto: CreateBrandDto) {
+    const brand: Brand = {
+      id: uuid(),
+      name: createBrandDto.name,
+      createAt: Date.now()
+    }
+
+    this.brands.push( brand )
+
+    return brand
+  }
+
+  findAll() {
+    return this.brands
+  }
+
+  findOne(id: string): Brand {
+    const brand = this.brands.find((brand) => brand.id === id)
+
+    if( !brand ) throw new NotFoundException()
+
+    return brand
+  }
+
+  update(id: string, { name }: UpdateBrandDto) {
+    const brand = this.findOne(id)
+
+    brand.updatedAt = Date.now()
+    brand.name = name
+
+    return brand
+  }
+
+  remove(id: string) {
+    const deleted = this.findOne(id)
+
+    this.brands = this.brands.filter( brand => brand.id !== id )
+    return deleted
+  }
+}
